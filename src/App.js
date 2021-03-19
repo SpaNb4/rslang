35ef31184a4b0eb03e691c778476f42be3c6a4fd
magklax <nadic2006@yandex.ru>
2021-03-19 20:49:52 +0100

    feat: add basic header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './components/Header/Header';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Header />
+		</div>
+	);
 }
 
 export default App;
