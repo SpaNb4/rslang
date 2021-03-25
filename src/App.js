ca118b4d1b8b6d60a94ccdb231ef9e909e24fb58
magklax <nadic2006@yandex.ru>
2021-03-25 07:37:39 +0100

    feat: add footer

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import Header from './components/Header/Header';
 import Features from './components/Features/Features';
 import Team from './components/Team/Team';
 import Video from './components/Video/Video';
-import { globalClasses as c } from './common/constants';
+import Footer from './components/Footer/Footer';
 import Book from './components/Book/Book';
 
 import { LocalStorageKeys } from './common/constants';
 import { login, register } from './store/app/actions';
 import { getUserId, getToken } from './store/app/slices';
 import { fetchUserWords } from './store/dictionary/actions';
+import { globalClasses as c } from './common/constants';
 
 function App() {
 	const dispatch = useDispatch();
@@ -56,6 +57,7 @@ function App() {
 					<Route path="/book/:group" component={Book} />
 				</Switch>
 			</main>
+			<Footer />
 		</React.Fragment>
 	);
 }
