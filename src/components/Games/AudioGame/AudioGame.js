a402452a7e6bbf92c21d13ecb6525867fe2c410c
magklax <nadic2006@yandex.ru>
2021-04-09 10:55:34 +0200

    fix: remove map

--- src/components/Games/AudioGame/AudioGame.js
+++ src/components/Games/AudioGame/AudioGame.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect, useRef } from 'react';
 import shuffle from 'lodash/shuffle';
 import sampleSize from 'lodash/sampleSize';
 import max from 'lodash/max';
-import map from 'lodash/map';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { AiFillSound } from 'react-icons/ai';
 import { ExternalUrls } from '../../../common/constants';
@@ -42,7 +41,7 @@ function AudioGame({ wordData }) {
 					correct: corrAnswersWords,
 					wrong: wrongAnswersWords,
 					streak: maxStreak,
-					words: map(resWords, 'word'),
+					words: resWords,
 				})
 			);
 		}
